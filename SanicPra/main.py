625a15526e6f2fcab76382410ec02076df6167d4
yoCkawa0 <18nc047@ms.dendai.ac.jp>
2021-10-20 16:14:08 +0900

    testing

--- SanicPra/main.py
+++ SanicPra/main.py
@@ -1,14 +1,25 @@
+from sanic import Sanic
 from sanic.response import text
 from sanic.response import json
-from sanic import Sanic
+from peewee import *
+import peewee as pe
+import psycopg2
 
-todoitems = [
-    {"id": 1, "title": "aaa"},
-    {"id": 2, "title": "bbb"},
-    {"id": 3, "title": "homeworks"},
-    {"id": 4, "title": "flask"},
-    {"id": 5, "title": "sanic"}
-]
+
+psql_db = PostgresqlDatabase(
+    'testdb', host='localhost', port=8881, user='postgres', password='example')
+
+
+class TodoItems(pe.Model):
+    todoTitle = pe.CharField()
+
+    class Meta:
+        database = psql_db
+        table_name = 'todo_items'
+
+
+psql_db.connect()
+psql_db.create_tables([TodoItems])
 
 
 app = Sanic(__name__)
@@ -19,36 +30,40 @@ async def test(request):
     return json({'hello': 'world'})
 
 
-@app.route('/posts', methods=['GET'])
-async def get_allitems(request):
-    return json(todoitems)
+@app.route('/todos', methods=['GET'])
+async def get_all_todes(request):
+    todos = TodoItems.select()
+    res = {
+        "todos": []
+    }
+    for todo in todos:
+        res["todos"].append({
+            "id": todo.id,
+            "todoTitle": todo.todoTitle
+        })
+    return json(res)
 
 
-@app.route('/posts/<id:int>', methods=['GET'])
-async def get_item(request, id):
-    return json(todoitems[id-1])
+@app.route('/todo/<id:int>', methods=['GET'])
+async def get_todo(request, id):
+    todotitle = TodoItems.get(TodoItems.id == id)
+    return json({id: todotitle.todoTitle})
 
 
-@app.route('/posts/add', methods=['POST'])
-async def add_item(request):
-    item = request.json
-    todoitems.append(item)
-    # print(todoitems)
-    return json(todoitems)
+@app.route('/todo', methods=['POST'])
+async def add_todo(request):
+    req = request.json["todoTitle"]
+    todoTitle = "{}".format(req)
+    print(req)
+    TodoItems.create(todoTitle=todoTitle)
+    return json({'!added': 'todo!'})
 
 
-@app.route('/posts/update/<id:int>', methods=['PUT'])
-async def update_item(request, id):
-    item = request.json
-    todoitems[id-1] = item
-    return json(todoitems)
-
-
-@app.route('/posts/delete/<id:int>', methods=['DELETE'])
-async def delete_item(request, id):
-    id -= 1
-    todoitems.pop(id)
-    return json(todoitems)
+@app.route('/todo/<id:int>', methods=['DELETE'])
+async def delete_todo(request, id):
+    todo = TodoItems.get(TodoItems.id == id)
+    todo.delete_instance()
+    return json({'!deleted': 'todo!'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ SanicPra/tests/test_sample.py
@@ -0,0 +1,77 @@
+import pytest
+from sanic import Sanic, response
+from sanic_testing import TestManager
+from sanic import Sanic
+from sanic.response import text
+from peewee import *
+import peewee as pe
+# import psycopg2
+import json as pyjson
+from ..main import *
+
+# psql_db = PostgresqlDatabase(
+#     'testdb', host='localhost', port=8881, user='postgres', password='example')
+
+
+@pytest.fixture(scope="session", autouse=True)
+def create_data():
+    with psql_db.atomic() as txn:
+        TodoItems.create_table()
+        TodoItems.insert_many([
+            {"id": 1, "todoTitle": "task1"},
+            {"id": 2, "todoTitle": "task2"},
+            {"id": 3, "todoTitle": "task1"},
+            {"id": 4, "todoTitle": "task2"},
+            {"id": 5, "todoTitle": "task100"},
+            {"id": 6, "todoTitle": "task100"},
+            {"id": 9, "todoTitle": "task100"},
+            {"id": 20, "todoTitle": "task100"},
+            {"id": 10, "todoTitle": "task100"},
+            {"id": 11, "todoTitle": "task100"}
+        ]).execute()
+
+        psql_db.execute_sql(
+            "SELECT setval('todo_items_id_seq', max(id)) FROM todo_items;"
+        )
+
+        yield
+        txn.rollback()
+
+
+def test_hello(request):
+    _, response = app.test_client.get('/')
+    print(response.json)
+    assert response.json == ({'hello': 'world'})
+
+
+def test_get_todo(request):
+    _, response = app.test_client.get('/todo/11')
+    print(response.json)
+    assert response.json == ({"11": "task100"})
+
+
+def test_get_all_todos(request):
+    _, response = app.test_client.get('/todos')
+    print(response.json)
+    assert response.json == {"todos": [{"id": 1, "todoTitle": "task1"}, {"id": 2, "todoTitle": "task2"}, {"id": 3, "todoTitle": "task1"}, {"id": 4, "todoTitle": "task2"}, {
+        "id": 5, "todoTitle": "task100"}, {"id": 6, "todoTitle": "task100"}, {"id": 9, "todoTitle": "task100"}, {"id": 20, "todoTitle": "task100"}, {"id": 10, "todoTitle": "task100"}, {"id": 11, "todoTitle": "task100"}]}
+
+
+def test_delete_todo(request):
+    _, response = app.test_client.delete('todo/11')
+    checktodo = TodoItems.get_or_none(TodoItems.id == 11)
+    assert checktodo == None
+
+
+def test_add_todo(request):
+    with psql_db.atomic()as txn:
+        assert len(TodoItems.select().where(
+            TodoItems.todoTitle == "task200")) == 0
+        data = {"todoTitle": "task200"}
+        _, response = app.test_client.post(
+            'todo', data=pyjson.dumps(data))
+        assert response.json == {'!added': 'todo!'}
+        assert len(TodoItems.select().where(
+            TodoItems.todoTitle == "task200")
+        ) == 1
+        txn.rollback()
