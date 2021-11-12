23a3e3bd157b05cc50ce7d891303058a40434895
yoCkawa0 <18nc047@ms.dendai.ac.jp>
2021-11-12 18:42:40 +0900

    Fix get_table's logic

--- api_sanic/api_sanic/api/get_table.py
+++ api_sanic/api_sanic/api/get_table.py
@@ -1,39 +1,10 @@
+from logging import NullHandler
 from api_sanic.api import app
 from sanic.response import json
 from api_sanic.models import TodoItem, User, ConnectTodo, Tag
 # from pprint import pprint
 # from peewee import JOIN
-
-
-@app.route('/todos', methods=['GET'])
-async def get_all_todes(request):
-    query = (
-        TodoItem.select(TodoItem.id, TodoItem.todo_title, User.id, User.user_name, Tag.id, Tag.tag_name)
-        .join(User)
-        .switch(TodoItem)
-        .join(ConnectTodo)
-        .join(Tag)
-    )
-
-    res = {
-        "todos": []
-    }
-
-    for q in query:
-        res["todos"].append({
-            "id": q.id,
-            "todo_title": q.todo_title,
-            "user": {
-                "id": q.user.id,
-                "name": q.user.user_name
-            },
-            "tag": {
-                "id": q.connecttodo.tag_id,
-                "name": q.connecttodo.tag.tag_name
-            }
-        })
-
-    return json(res)
+from sanic.exceptions import SanicException
 
 
 @app.route('/todo/<id:int>', methods=['GET'])
@@ -46,23 +17,71 @@ async def get_todo(request, id):
         .join(Tag)
         .where(TodoItem.id == id)
     )
+    if len(query) == 0:
+        raise SanicException("IDが一致するtodoが見つかりませんでした.", status_code=404)
+        # return json({"message": "There's no todo!"})
 
-    res = {
-        "todo": []
-    }
-
+    todo = {}
     for q in query:
-        res["todo"].append({
-            "id": q.id,
-            "todo_title": q.todo_title,
-            "user": {
-                "id": q.user.id,
-                "name": q.user.user_name
-            },
-            "tag": {
+        if q.id not in todo:
+            todo[q.id] = {
+                "id": q.id,
+                "todo_title": q.todo_title,
+                "user": {
+                    "id": q.user.id,
+                    "name": q.user.user_name
+                },
+                "tag": [{
+                    "id": q.connecttodo.tag_id,
+                    "name": q.connecttodo.tag.tag_name
+                }]
+            }
+        else:
+            todo[q.id]["tag"].append({
                 "id": q.connecttodo.tag_id,
                 "name": q.connecttodo.tag.tag_name
-            }
-        })
+            })
 
+    res = {
+        "todo": [v for _, v in todo.items()]
+    }
+    # for k, v in todo.items():
+    #     res["todo"].append(v)
+
+    return json(res)
+
+
+@app.route('/todos', methods=['GET'])
+async def get_all_todes(request):
+    query = (
+        TodoItem.select(TodoItem.id, TodoItem.todo_title, User.id, User.user_name, Tag.id, Tag.tag_name)
+        .join(User)
+        .switch(TodoItem)
+        .join(ConnectTodo)
+        .join(Tag)
+    )
+    todos = {}
+    for q in query:
+        if q.id not in todos:
+            todos[q.id] = {
+                "id": q.id,
+                "todo_title": q.todo_title,
+                "user": {
+                    "id": q.user.id,
+                    "name": q.user.user_name
+                },
+                "tag": [{
+                    "id": q.connecttodo.tag_id,
+                    "name": q.connecttodo.tag.tag_name
+                }]
+            }
+        else:
+            todos[q.id]["tag"].append({
+                "id": q.connecttodo.tag_id,
+                "name": q.connecttodo.tag.tag_name
+            })
+
+    res = {
+        "todos": [v for _, v in todos.items()]
+    }
     return json(res)
